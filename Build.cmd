1b077894ca189c94dc450c9d47590289f13ffcf3
ClAndrew <cmlandrews@gmail.com>
2022-04-26 00:14:02 -0700

    Initial add

---
+++ Build.cmd
@@ -0,0 +1,4 @@
+echo on
+echo "ASDF"
+echo d:\64tass\64tass --long-address --flat -b --m65816 --intel-hex -o hello.hex --list=hello.lst --labels=hello.lbl D:\repos\foenix\hello\programs\hello\hello.s
+copy /Y D:\repos\foenix\hello\programs\hello\Debug\hello.hex "D:\Program Files\C256 Foenix Project\FoenixIDE Setup"
