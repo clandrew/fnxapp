9732a3c9a02326faf2b1606815bdb794625bc310
ClAndrew <cmlandrews@gmail.com>
2022-04-29 21:06:30 -0700

    Save

--- hello.s
+++ hello.s
@@ -9,30 +9,29 @@
 ; Main code
 * = $02000
 
+; Function: Main
+; Postconditions:
+;       - Return long.
+;       - Processor bits are unchanged.
+;       - Data bank is unchanged.
+;
 START           PHB
                 PHP
-
-                REP #$30                ; A, X, and Y are 16-bit
-                .al
-                .xl
 
                 SEP #$20                ; A is 8-bit
                 .as
                 REP #$10                ;X, and Y are 16-bit
                 .xl
 
-                LDX #<>GREETING         ; Point to GREETING
-                LDA #`GREETING
+                LDX #<>GREETING         ; X=address of GREETING
+                LDA #`GREETING          ; A=data bank of GREETING
                 PHA
-                PLB
+                PLB                     ; Both X and data bank are set
                 JSL PUTS                ; And print it
 
-                REP #$20                ; A is 16-bit
-                .al
+                LDA #$00                ; Set a return value of 0
 
-                LDA #$1234              ; Set a return value of $1234
-
-                PLP
+                PLP                     ; Restore data bank and processor flags
                 PLB
                 RTL                     ; Go back to the caller
 
