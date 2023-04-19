b6ab9c067b73718040839c1099ae257061659ab6
ClAndrew <cmlandrews@gmail.com>
2023-04-18 22:52:43 -0700

    Add pgx sample

---
+++ exec/exec.s
@@ -0,0 +1,32 @@
+.include "platform.s"
+
+; Place a PGX header before the code section
+* = START - 8
+                .text "PGX"
+                .byte $01
+                .dword START
+
+; Code section
+* = $020000
+
+START           PHB
+                PHP
+
+                SEP #$20                ; A is 8-bit
+                .as
+                REP #$10                ;X, and Y are 16-bit
+                .xl
+
+                LDX #<>GREETING         ; Point to GREETING
+                LDA #`GREETING
+                PHA
+                PLB
+                JSL PUTS                ; And print it
+                                
+                LDA #$0                 ; Set a return value of 0
+
+                PLP
+                PLB
+                RTL                     ; Go back to the caller
+
+GREETING        .null "Hello, world!", 13 
