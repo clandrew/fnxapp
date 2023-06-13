b18eacd81293cc44aa337a106a189b256c9aceaa
ClAndrew <cmlandrews@gmail.com>
2023-06-12 22:21:04 -0700

    Add rainbowtext proj

---
+++ rainbowtext/rsrc/textcolors.s
@@ -0,0 +1,19 @@
+TEXT_LUT_START
+.byte $12, $06, $E3, $00
+.byte $01, $88, $F5, $00
+.byte $01, $CB, $FE, $00
+.byte $13, $C7, $A1, $00
+.byte $32, $AF, $4E, $00
+.byte $EA, $BE, $43, $00
+.byte $C6, $7C, $00, $00
+.byte $9A, $67, $9E, $00
+.byte $12, $06, $E3, $00
+.byte $01, $88, $F5, $00
+.byte $01, $CB, $FE, $00
+.byte $13, $C7, $A1, $00
+.byte $32, $AF, $4E, $00
+.byte $EA, $BE, $43, $00
+.byte $C6, $7C, $00, $00
+.byte $9A, $67, $9E, $00
+
+TEXT_LUT_END = *
